94460d0a924b27e57cea2dc990c5ddc339aeb2b4
Loris Sigrist <sigrist@hasch.io>
2023-04-20 19:34:03 +0200

    Repeat promise tests like the other ones

--- tests/promises.test.ts
+++ tests/promises.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, test } from "vitest";
 import { z } from "zod";
 import { zocker } from "../src";
 
+const repeats = 100;
+
 const promise_schemas = {
     "promise of number": z.promise(z.number()),
     "promise of string": z.promise(z.string()),
@@ -18,7 +20,9 @@ describe("Promise generation", () => {
     test.each(schema_keys)("generates valid data for %s", async (key) => {
         const schema = promise_schemas[key];
         const generate = zocker(schema);
-        const data = generate();
-        await expect(schema.parseAsync(data)).resolves.toEqual(await data);
+        for (let i = 0; i < repeats; i++) {
+            const data = generate();
+            await expect(schema.parseAsync(data)).resolves.toEqual(await data);
+        }
     });
 });
