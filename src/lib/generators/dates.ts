db18638c79f85b49b7cd351bda82c31d94465c43
Loris Sigrist <loris@sigrist.dev>
2025-04-24 19:53:14 +0200

    Migrate `dates` generator to zod 4

--- src/lib/generators/dates.ts
+++ src/lib/generators/dates.ts
@@ -2,11 +2,26 @@ import { faker } from "@faker-js/faker";
 import { InvalidSchemaException } from "../exceptions.js";
 import { Generator } from "lib/generate.js";
 import { InstanceofGeneratorDefinition } from "lib/zocker.js";
-import { z } from "zod";
+import * as z from "@zod/core";
 
-const generate_date: Generator<z.ZodDate> = (date_schema, ctx) => {
-	const min = get_date_check(date_schema, "min")?.value ?? null;
-	const max = get_date_check(date_schema, "max")?.value ?? null;
+// The minimum & maximum date 
+// according to https://262.ecma-international.org/5.1/#sec-15.9.1.1
+const MIN_DATE = new Date(-8640000000000000);
+const MAX_DATE = new Date(8640000000000000);
+
+const generate_date: Generator<z.$ZodDate> = (date_schema, ctx) => {
+	const min_checks = date_schema._zod.def.checks?.filter(c => c instanceof z.$ZodCheckGreaterThan) ?? [];
+	const max_checks = date_schema._zod.def.checks?.filter(c => c instanceof z.$ZodCheckLessThan) ?? [];
+	
+	const min = min_checks.reduce((acc, check) => {
+		const value = check._zod.def.value as Date;
+		return value > acc ? value : acc;
+	}, MIN_DATE);
+
+	const max = max_checks.reduce((acc, check) => {
+		const value = check._zod.def.value as Date;
+		return value < acc ? value : acc;
+	}, MAX_DATE);
 
 	if (min && max && max < min)
 		throw new InvalidSchemaException("max date is less than min date");
@@ -17,18 +32,8 @@ const generate_date: Generator<z.ZodDate> = (date_schema, ctx) => {
 	});
 };
 
-function get_date_check<Kind extends z.ZodDateCheck["kind"]>(
-	schema: z.ZodDate,
-	kind: Kind
-): Extract<z.ZodDateCheck, { kind: Kind }> | undefined {
-	const check = schema._def.checks.find((check) => check.kind === kind) as
-		| Extract<z.ZodDateCheck, { kind: Kind }>
-		| undefined;
-	return check;
-}
-
-export const DateGenerator: InstanceofGeneratorDefinition<z.ZodDate> = {
-	schema: z.ZodDate as any,
+export const DateGenerator: InstanceofGeneratorDefinition<z.$ZodDate> = {
+	schema: z.$ZodDate as any,
 	generator: generate_date,
 	match: "instanceof"
 };
