33d886eb0c92e0a84b3e4751e52fc373a4ca7003
Loris Sigrist <loris@sigrist.dev>
2025-04-24 20:53:16 +0200

    Update Tuple

--- src/lib/generators/tuple.ts
+++ src/lib/generators/tuple.ts
@@ -1,16 +1,16 @@
 import { InstanceofGeneratorDefinition } from "../zocker.js";
 import { generate, Generator } from "../generate.js";
-import { z } from "zod";
+import * as z from "@zod/core";
 
-const generate_tuple: Generator<z.ZodTuple> = (schema, generation_context) => {
-	const tuple = schema._def.items.map(<Z extends z.ZodSchema>(item: Z) =>
+const generate_tuple: Generator<z.$ZodTuple> = (schema, generation_context) => {
+	const tuple = schema._zod.def.items.map((item) =>
 		generate(item, generation_context)
 	);
 	return tuple as z.infer<typeof schema>;
 };
 
-export const TupleGenerator: InstanceofGeneratorDefinition<z.ZodTuple> = {
-	schema: z.ZodTuple as any,
+export const TupleGenerator: InstanceofGeneratorDefinition<z.$ZodTuple> = {
+	schema: z.$ZodTuple as any,
 	generator: generate_tuple,
 	match: "instanceof"
 };
