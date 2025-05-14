3399f645b2c35dc7f026b91f717e5811d05f2387
Loris Sigrist <loris@sigrist.dev>
2025-05-14 08:11:42 +0200

    Make CUID work

--- src/lib/generators/cuid.ts
+++ src/lib/generators/cuid.ts
@@ -2,11 +2,12 @@ import { InstanceofGeneratorDefinition } from "lib/zocker.js";
 import { Generator, generate } from "../generate.js";
 import * as z from "@zod/core";
 import { getLengthConstraints } from "./string/length-constraints.js";
-import { InvalidSchemaException } from "lib/exceptions.js";
+import { InvalidSchemaException } from "../exceptions.js";
 import { getContentConstraints } from "./string/content-constraints.js";
+import { faker } from "@faker-js/faker";
 
-const CUID_REGEX = /^c[^\s-]{8,}$/i;
 const CUID_MIN_LENGTH = 9;
+const CUID_COMMON_LENGHT = 25; // Most CUIDs are around 25 characters long
 
 
 /**
@@ -39,10 +40,12 @@ const generate_cuid: Generator<z.$ZodCUID> = (
         throw new InvalidSchemaException("CUID must start with a 'c'");
     }
 
+    const length = length_constraints.exact ?? faker.datatype.number({
+        min: Math.max(length_constraints.min, CUID_MIN_LENGTH),
+        max: length_constraints.max == Infinity ? length_constraints.min + CUID_COMMON_LENGHT : length_constraints.max
+    });
 
-
-    console.log(schema._zod.def);
-    return "cjld2cyuq0000t3rmniod1foy";
+    return generateCUIDofLength(length);
 };
 
 export const CUIDGenerator: InstanceofGeneratorDefinition<
@@ -53,3 +56,15 @@ export const CUIDGenerator: InstanceofGeneratorDefinition<
     match: "instanceof"
 };
 
+/**
+ * Generates a CUID of the given length
+ * @param len The length of the CUID. Must be at least 9
+ * @returns A CUID of the given length
+ * @throws 
+ */
+function generateCUIDofLength(len: number) { 
+    if(len < CUID_MIN_LENGTH) throw new TypeError("CUID must be at least 9 characters long");
+    
+    let cuid = "c" + faker.random.alphaNumeric( len - 1);
+    return cuid;
+}
