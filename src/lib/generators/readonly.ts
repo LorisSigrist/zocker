1d6550195441c41df71a7a22862bf05d13e486f2
Loris Sigrist <loris.sigrist@hotmail.com>
2025-05-16 10:31:02 +0200

    Migrate `z.readonly`to zod 4

--- src/lib/generators/readonly.ts
+++ src/lib/generators/readonly.ts
@@ -1,9 +1,9 @@
 import { Generator, generate } from "../generate.js";
 import { InstanceofGeneratorDefinition } from "../zocker.js";
-import { z } from "zod";
+import * as z from "zod/v4/core"
 
-const generate_readonly: Generator<z.ZodReadonly<any>> = (readonly_schema, ctx) => {
-	const inner = generate(readonly_schema._def.innerType, ctx);
+const generate_readonly: Generator<z.$ZodReadonly<any>> = (readonly_schema, ctx) => {
+	const inner = generate(readonly_schema._zod.def.innerType, ctx);
 
 	if(typeof inner === "object") {
         Object.freeze(inner);
@@ -13,9 +13,9 @@ const generate_readonly: Generator<z.ZodReadonly<any>> = (readonly_schema, ctx) 
 };
 
 export const ReadonlyGenerator: InstanceofGeneratorDefinition<
-    z.ZodReadonly<any>
+    z.$ZodReadonly<any>
     > = {
-    schema: z.ZodReadonly as any,
+    schema: z.$ZodReadonly as any,
     generator: generate_readonly,
     match: "instanceof"
 };
