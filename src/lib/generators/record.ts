7a9b6a648925b691c5be9dcbbf3c4eeca6d7c1cf
Loris Sigrist <loris.sigrist@hotmail.com>
2025-05-17 20:46:06 +0200

    Fix any & unknown generators

--- src/lib/generators/record.ts
+++ src/lib/generators/record.ts
@@ -5,41 +5,40 @@ import { RecursionLimitReachedException } from "../exceptions.js";
 import { InstanceofGeneratorDefinition } from "../zocker.js";
 
 export type RecordOptions = {
-	max: number;
-	min: number;
+  max: number;
+  min: number;
 };
 
 const generate_record: Generator<z.$ZodRecord> = (schema, ctx) => {
-	type Key = z.infer<(typeof schema)["_zod"]["def"]["keyType"]>;
-	type Value = z.infer<(typeof schema)["_zod"]["def"]["valueType"]>;
+  type Key = z.infer<(typeof schema)["_zod"]["def"]["keyType"]>;
+  type Value = z.infer<(typeof schema)["_zod"]["def"]["valueType"]>;
 
-	const record = {} as any as Record<Key, Value>;
+  const record = {} as any as Record<Key, Value>;
 
-	try {
-		const keys = generateKeys<typeof schema>(schema, ctx) as unknown as Key[];
+  try {
+    const keys = generateKeys<typeof schema>(schema, ctx) as unknown as Key[];
 
-		for (const key of keys) {
-			let value: Value;
-			let prev_semantic_context = ctx.semantic_context;
+    for (const key of keys) {
+      let value: Value;
+      let prev_semantic_context = ctx.semantic_context;
 
-			try {
-				ctx.path.push(key);
-				ctx.semantic_context = "key";
+      try {
+        ctx.path.push(key);
+        ctx.semantic_context = "key";
 
-				value = generate(schema._zod.def.valueType, ctx) as Value;
-			} finally {
-				ctx.path.pop();
-				ctx.semantic_context = prev_semantic_context;
-			}
+        value = generate(schema._zod.def.valueType, ctx) as Value;
+      } finally {
+        ctx.path.pop();
+        ctx.semantic_context = prev_semantic_context;
+      }
+      record[key] = value;
+    }
+  } catch (error) {
+    if (error instanceof RecursionLimitReachedException) return record;
+    throw error;
+  }
 
-			record[key] = value;
-		}
-	} catch (error) {
-		if (error instanceof RecursionLimitReachedException) return record;
-		throw error;
-	}
-
-	return record;
+  return record;
 };
 
 /**
@@ -48,34 +47,34 @@ const generate_record: Generator<z.$ZodRecord> = (schema, ctx) => {
  * @param ctx 
  */
 function generateKeys<Z extends z.$ZodRecord>(
-	schema: Z,
-	ctx: GenerationContext<z.$ZodRecord>
+  schema: Z,
+  ctx: GenerationContext<z.$ZodRecord>
 ): z.infer<Z>[] {
-	const keySchema = schema._zod.def.keyType;
+  const keySchema = schema._zod.def.keyType;
 
-	// Enums & other schemas with a list of values must always return ALL
-	// their values
-	if (keySchema._zod.values) {
-		return [...keySchema._zod.values] as z.infer<Z>[];
-	}
+  // Enums & other schemas with a list of values must always return ALL
+  // their values
+  if (keySchema._zod.values) {
+    return [...keySchema._zod.values] as z.infer<Z>[];
+  }
 
-	// otherwise, pick a random number of keys
-	const numKeys = faker.datatype.number({
-		min: ctx.record_options.min,
-		max: ctx.record_options.max
-	});
+  // otherwise, pick a random number of keys
+  const numKeys = faker.datatype.number({
+    min: ctx.record_options.min,
+    max: ctx.record_options.max
+  });
 
-	const keys: z.infer<Z>[] = [];
-	while(keys.length < numKeys) {
-		const key = generate(schema._zod.def.keyType, ctx) as unknown as z.infer<Z>;
-		if(key != undefined) keys.push(key);
-	}
+  const keys: z.infer<Z>[] = [];
+  while (keys.length < numKeys) {
+    const key = generate(schema._zod.def.keyType, ctx) as unknown as z.infer<Z>;
+    if (key != undefined) keys.push(key);
+  }
 
-	return keys;
+  return keys;
 }
 
 export const RecordGenerator: InstanceofGeneratorDefinition<z.$ZodRecord> = {
-	schema: z.$ZodRecord as any,
-	generator: generate_record,
-	match: "instanceof"
+  schema: z.$ZodRecord as any,
+  generator: generate_record,
+  match: "instanceof"
 };
--- tests/record.test.ts
+++ tests/record.test.ts
@@ -3,17 +3,17 @@ import { z } from "zod/v4";
 import { test_schema_generation } from "./utils";
 
 const record_schemas = {
-	"record of numbers": z.record(z.coerce.number() as any, z.number()),
-	"record with enum": z.record(z.enum(["a", "b", "c"]), z.number()), // zod 4 expects all enum keys to be exhausted
-	"record with enum (partial)": z.partialRecord(z.enum(["a", "b", "c"]), z.number()), // zod 4 expects all enum keys to be exhausted
-	"record of objects": z.record(z.string(), z.object({ b: z.string() })),
-	"record of objects with strings with regex pattern": z.record(
-		z.string().regex(/^[0-9]+$/),
-		z.object({ b: z.string() })
-	)
-	
+  "record of numbers": z.record(z.coerce.number() as any, z.number()),
+  "record with enum": z.record(z.enum(["a", "b", "c"]), z.number()), // zod 4 expects all enum keys to be exhausted
+  "record with enum (partial)": z.partialRecord(z.enum(["a", "b", "c"]), z.number()), // zod 4 expects all enum keys to be exhausted
+  "record of objects": z.record(z.string(), z.object({ b: z.string() })),
+  "record of objects with strings with regex pattern": z.record(
+    z.string().regex(/^[0-9]+$/),
+    z.object({ b: z.string() })
+  ),
+  "record with symbol keys": z.record(z.symbol(), z.number())
 } as const;
 
 describe("Record generation", () => {
-	test_schema_generation(record_schemas);
+  test_schema_generation(record_schemas);
 });
