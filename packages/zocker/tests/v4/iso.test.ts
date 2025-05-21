d8b0c9f15081cc491eb22e4788f5b33a87aed62b
Loris Sigrist <loris@sigrist.dev>
2025-05-21 20:10:53 +0200

    Add more test for ISO Date & Datetime

--- packages/zocker/tests/v4/iso.test.ts
+++ packages/zocker/tests/v4/iso.test.ts
@@ -2,18 +2,41 @@ import { describe } from "vitest";
 import { z } from "zod/v4";
 import { test_schema_generation } from "./utils";
 
-const iso_schemas = {
-	"iso date": z.iso.date(),
+const iso_datetime_schemas = {
 	"iso datetime": z.iso.datetime(),
-	"iso time": z.iso.time(),
-	"iso duration": z.iso.duration(),
-
 	"iso datetime with offset": z.iso.datetime({ offset: true }),
 	"iso datetime with precision": z.iso.datetime({ precision: 2 }),
 	"iso datetime with precision 0": z.iso.datetime({ precision: 0 }),
 	"iso datetime with precision and offset": z.iso.datetime({ precision: 2, offset: true })
 };
 
-describe("ISO generation", () => {
-	test_schema_generation(iso_schemas);
+const iso_date_schemas = {
+	"iso date": z.iso.date(),
+};
+
+const iso_time_schemas = {
+	"iso time": z.iso.time(),
+	"iso time with precision": z.iso.time({ precision: 7 }),
+	"iso time with precision 0": z.iso.time({ precision: 0 })
+};
+
+const iso_duration_schemas = {
+	"iso duration": z.iso.duration(),
+	"iso duration with pattern": z.iso.duration({ pattern: z.regexes.duration }),
+};
+
+describe("ISO Datetime generation", () => {
+	test_schema_generation(iso_datetime_schemas);
+});
+
+describe("ISO Date generation", () => {
+	test_schema_generation(iso_date_schemas);
+});
+
+describe("ISO Time generation", () => {
+	test_schema_generation(iso_time_schemas);
+});
+
+describe("ISO Duration generation", () => {
+	test_schema_generation(iso_duration_schemas);
 });
