f9d074dbbed932b758b527567fb0541bb04c0fc1
Loris Sigrist <loris@sigrist.dev>
2025-05-21 21:03:47 +0200

    Add test for all legacy string formats

--- packages/zocker/tests/v4/deprecated-string-formats.test.ts
+++ packages/zocker/tests/v4/deprecated-string-formats.test.ts
@@ -6,6 +6,7 @@ const formatted_string_schemas = {
     "string.date": z.string().date(),
     "string.datetime": z.string().datetime(),
     "string.duration": z.string().duration(),
+     "string.time": z.string().time(),
     "string.email": z.string().email(),
     "string.guid": z.string().guid(),
     "string.uuid": z.string().uuid(),
@@ -17,6 +18,16 @@ const formatted_string_schemas = {
     "string.cuid2": z.string().cuid2(),
     "string.ulid": z.string().ulid(),
     "string.emoji": z.string().emoji(),
+    "string.xid": z.string().xid(),
+    "string.nanoid": z.string().nanoid(),
+    "string.ksuid": z.string().ksuid(),
+    "string.ipv6": z.string().ipv6(),
+    "string.ipv4": z.string().ipv4(),
+    "string.e164": z.string().e164(),
+    "string.cidrv4": z.string().cidrv4(),
+    "string.cidrv6": z.string().cidrv6(),
+    "string.base64url": z.string().base64url(),
+    "string.base64": z.string().base64(),
 } as const;
 
 describe("Deprecated string formats", () => {
