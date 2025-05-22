713881066b575aadd0a7943ec99caf68be565b61
Loris Sigrist <loris@sigrist.dev>
2025-05-22 11:00:39 +0200

    Add test for `z.dates`

--- packages/zocker/tests/v4/dates.test.ts
+++ packages/zocker/tests/v4/dates.test.ts
@@ -4,6 +4,7 @@ import { test_schema_generation } from "./utils";
 
 const date_schemas = {
 	"plain date": z.date(),
+	"date with min": z.date().min(new Date("2055-01-01")),
 	"date with max": z.date().max(new Date("2021-01-01")),
 	"date with min and max": z
 		.date()
